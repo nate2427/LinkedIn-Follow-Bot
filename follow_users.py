ca0edba50257a32ec173d5fac4b166f58a4cd21f
nate2427 <natebaker2427@gmail.com>
2023-01-18 07:23:32 -0500

    added user agent to bypass the headless checking.

--- follow_users.py
+++ follow_users.py
@@ -14,8 +14,11 @@ def set_up_web_driver():
     # check if local or production
     if os.getenv("PRODUCTION") == "True":
         # set up web driver
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         chrome_options = webdriver.ChromeOptions()
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument(f'user-agent={user_agent}')
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
@@ -24,6 +27,8 @@ def set_up_web_driver():
         print("Headless Chrome Initialized")
     else:
         chrome_options = webdriver.ChromeOptions()
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+        chrome_options.add_argument(f'user-agent={user_agent}')
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--window-size=1920,1080")
         driver = webdriver.Chrome(chrome_options=chrome_options)
